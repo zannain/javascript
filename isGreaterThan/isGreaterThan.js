a08877f8b444822323645b3d9defb19f7bc0b445
zannain <zannainshafi@gmail.com>
2017-08-18 22:16:31 -0400

    isGreaterThan10

---
+++ isGreaterThan/isGreaterThan.js
@@ -0,0 +1,4 @@
+function isGreaterThan(num1, num2) {
+  if (num2 > num1) return true;
+  else return false;
+}
---
+++ isGreaterThanTen/isGreaterThanTen.js
@@ -0,0 +1,4 @@
+function isGreaterThanTen(num) {
+  if (num > 10) return true;
+  else return false;
+}

9fa6086b93ead346042b5b8a65577d3545a1e1d5
zannain <zannainshafi@gmail.com>
2017-08-18 22:24:31 -0400

    modulo

---
+++ modulo/modulo.js
@@ -0,0 +1,20 @@
+function modulo(num1, num2) {
+  if (isNaN(num1))
+    return NaN;
+
+  mod = 0;
+
+  if (Math.abs(num1) > 0) {
+    d = Math.floor(Math.abs(num1) / Math.abs(num2));
+    mod = Math.abs(num1) - (Math.abs(num2) * d);
+  }
+  if (num1 < 0) {
+    mod = -1 * mod;
+  }
+
+
+  return mod;
+}
+
+var output = modulo(-1, 2);
+console.log(output); // --> 1
